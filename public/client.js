c24705b9fb18178ec7ab76b77ecf01dbe568e837
kalleby <kalleby.santos38@gmail.com>
2020-10-25 19:59:12 +0000

    Feat: Local video streaming

--- public/client.js
+++ public/client.js
@@ -1,2 +1,31 @@
-// eslint-disable-next-line no-undef
-console.log(ROOMID);
+let roomID;
+const myVideo = document.createElement('video');
+const videoGrid = document.querySelector('.video-grid');
+
+const midiaConstraints = {
+  video: true,
+  audio: true,
+};
+
+// Creates a new video element and load the data stream
+const addVideoStream = (stream, element) => {
+  const video = element || document.createElement('video');
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => video.play());
+  videoGrid.append(video);
+};
+
+// Receives user stream data inputs
+const streaming = () => {
+  navigator.mediaDevices.getUserMedia(midiaConstraints)
+    .then((myStream) => {
+      myVideo.muted = true;
+      addVideoStream(myStream, myVideo);
+    });
+};
+
+function start(room) {
+  roomID = room;
+  console.log(roomID);
+  streaming();
+}
